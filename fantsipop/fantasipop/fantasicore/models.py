daabd44ba82d43642815354b6f154a8fdf454f8e
Jeipi12 <jua.valenzuelam@duocuc.cl>
2021-10-28 16:58:16 -0300

    Revert "aa"
    This reverts commit 42c54c5aea59c5d41cdad2c4428ae3d417c0c1ef.

--- fantsipop/fantasipop/fantasicore/models.py
+++ fantsipop/fantasipop/fantasicore/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-class Tipo_usuario(models.Model):
-    id_tipo_usuario = model.AutoField(primary_key=True)
-    nombre_tip = models.CharField(max_length=30)
-
-    def _str_(self):
-        return self.nombre
-
-class Usuario(models.Model):
-    num_run = models.IntegerField(primary_key=True)
-    dv_run = models.CharField(max_length=1,null=True)
+# Create your models here.
