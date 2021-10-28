42c54c5aea59c5d41cdad2c4428ae3d417c0c1ef
Jeipi12 <jua.valenzuelam@duocuc.cl>
2021-10-28 16:44:50 -0300

    aa
    aa

--- fantsipop/fantasipop/fantasicore/models.py
+++ fantsipop/fantasipop/fantasicore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Tipo_usuario(models.Model):
+    id_tipo_usuario = model.AutoField(primary_key=True)
+    nombre_tip = models.CharField(max_length=30)
+
+    def _str_(self):
+        return self.nombre
+
+class Usuario(models.Model):
+    num_run = models.IntegerField(primary_key=True)
+    dv_run = models.CharField(max_length=1,null=True)
